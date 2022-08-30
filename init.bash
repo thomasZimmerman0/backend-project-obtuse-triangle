2cef2c55fb545c22fde39dda5c730c9f01c5e777
Tommy Zimmerman <tomzimmerman7@protonmail.com>
2022-08-30 13:05:32 -0400

    lol

---
+++ init.bash
@@ -0,0 +1,5 @@
+# sequelize model:generate --name users --attributes userName:string,password:string,email:string,roleID:integer
+
+# sequelize model:generate --name roles --attributes name:string
+
+# sequelize model:generate --name drawings --attributes title:string,userID:integer,is_published:boolean
---
+++ migrations/20220830165603-create-users.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userName: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      roleID: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('users');
+  }
+};
