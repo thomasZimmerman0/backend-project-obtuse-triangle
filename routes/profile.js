41b04de37ca0055678ede2e6bcd9413b17386172
Tommy Zimmerman <tomzimmerman7@protonmail.com>
2022-09-01 16:01:07 -0400

    working accounts page and following functionality

--- routes/profile.js
+++ routes/profile.js
@@ -3,45 +3,109 @@ const router = express.Router();
 const auth = require('../auth')
 let db = require('../models')
 
-const path = require('path')
-const multer = require('multer')
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, 'profile_images')
-    },
-    filename: (req, file, cb) => {
-        console.log(file)
-        cb(null, Date.now() + path.extname(file.originalname))
-    }
-})
+// const path = require('path')
+// const multer = require('multer')
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//         cb(null, 'profile_images')
+//     },
+//     filename: (req, file, cb) => {
+//         console.log(file)
+//         cb(null, Date.now() + path.extname(file.originalname))
+//     }
+// })
 
 
-const upload = multer({storage: storage})
+// const upload = multer({storage: storage})
 
-router.get('/profile',auth, (req,res) => {
+// router.get('/profile',auth, (req,res) => {
 
    
-    res.render('profile', {
+//     res.render('profile', {
 
-        username: req.user.userName,
-        email: req.user.email
+//         username: req.user.userName,
+//         email: req.user.email
         
+//     })
+
+// })
+
+router.get('/profile/:id', async(req, res) => {
+
+    let selectedID = req.params.id
+    const user = await db.users.findByPk(selectedID)
+
+    // console.log(user.userName);
+
+    let followerString = user.followers
+    let followerArray = []
+
+    if(followerString != null){
+        followerArray = followerString.split(', ')
+    }
+
+    let followingString = user.following
+    let followingArray = []
+
+    if(followingString != null){
+        followingArray = followingString.split(', ')
+    }
+
+    console.log(followerArray);
+    console.log(followingArray)
+
+    res.render('profile', {
+        user : user,
+        followers : followerArray,
+        following : followingArray
     })
-
-})
-
-router.post("/profile", upload.single("image"), (req, res) =>{
-    res.render('profile')
-})
-
-router.get("/follow", auth, async (req, res)=>{
     
-    console.log('hello');
-    let updateFollow = await db.users.update({ followers: "1"}, {
+    
+})
+
+// router.post("/profile", upload.single("image"), (req, res) =>{
+//     res.render('profile')
+// })
+
+router.get("/follow/:id", auth, async (req, res)=>{
+
+    let selectedID = req.params.id
+    const user = await db.users.findByPk(selectedID)
+    let followerString = user.followers
+
+    followerString += req.user.id + ', '
+
+    let updateFollow = await db.users.update({ followers: followerString}, {
         where: {
-            userName: req.user.userName
+            userName: user.userName
         }
     })
+
+    let followingString = req.user.following
+
+    followingString += user.id + ', '
+
+    let updateFollowers = await db.users.update({ following: followingString}, {
+        where: {
+            userName : req.user.userName
+        }
+    })
+
+    res.redirect(`/profile/${selectedID}`)
+})
+
+router.get('/delete/:id', async (req, res)=>{
+
+    let selectedID = req.params.id
+
+
+    let deleteAccount = await db.users.destroy({
+        where: {
+            id : selectedID
+        }
+    })
+
+    res.redirect('/')
 })
 
 
