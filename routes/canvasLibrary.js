738fcafe6fffe5f0073e2ff9df699a95f9a42eac
RKovl <ryan.kovlaske@gmail.com>
2022-09-04 23:40:21 -0700

    everything sucks, nothing matters

---
+++ routes/canvasLibrary.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../auth')
+let db = require('../models')
+
+
+router.get('/drawings/', auth, async (req, res)=>{
+        let selectedID = req.params.id
+    try {
+        const drawings = await db.drawings.findAll({where:{userID: selectedID}})
+        console.log(drawings)
+        res.render('canvasLibrary', {
+            drawings: drawings
+        })
+    } 
+    catch (error) {
+        res.render('canvasLibrary')
+    }
+
+
+    // if(drawings.length == 0){
+    //     drawings[0] = 0
+    // }
+    // console.log(drawings)
+
+})
+
+router.post('/drawings/:id', async (req, res)=>{
+
+    let drawing = req.params.drawing
+
+    res.redirect('/'+drawing)
+})
+
+
+module.exports = router;
